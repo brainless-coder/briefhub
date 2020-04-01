141cf3f7b02684260d7798631977fb145359d878
Aditya Tomar <aditya001tomar@gmail.com>
2020-04-01 22:53:41 +0530

    added login signup functionalities

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,32 +2,49 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const {email, password} = formData;
+    const onChange = e => setFormData({...formData, name: e.target.value});
+
+    const onSubmit = e => {
+        
+            console.log(formData);
+       
+    }
     return (
         <Fragment>
-            <div class="alert alert-danger">
+            <div className="alert alert-danger">
                 Invalid credentials
             </div>
-            <h1 class="large text-primary">Sign In</h1>
-            <p class="lead"><i class="fas fa-user"></i> Sign into Your Account</p>
-            <form class="form" action="dashboard.html">
-                <div class="form-group">
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)} >
+                <div className="form-group">
                 <input
                     type="email"
                     placeholder="Email Address"
                     name="email"
+                    value={email}
+                    onChange={e => onChange(e)}
                     required
                 />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                 <input
                     type="password"
                     placeholder="Password"
                     name="password"
+                    value={password}
+                    onChange={e => onChange(e)}
                 />
                 </div>
-                <input type="submit" class="btn btn-primary" value="Login" />
+                <input type="submit" className="btn btn-primary" value="Login" />
             </form>
-            <p class="my-1">
+            <p className="my-1">
                 Don't have an account? <Link to="/register">Sign Up</Link>
             </p>
         </Fragment>
