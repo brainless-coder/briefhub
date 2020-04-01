d62bc837a6533c73e53f53821319e06d71b5238d
Aditya Tomar <aditya001tomar@gmail.com>
2020-04-01 15:50:13 +0530

    Added login signup page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <Fragment>
+            <div class="alert alert-danger">
+                Invalid credentials
+            </div>
+            <h1 class="large text-primary">Sign In</h1>
+            <p class="lead"><i class="fas fa-user"></i> Sign into Your Account</p>
+            <form class="form" action="dashboard.html">
+                <div class="form-group">
+                <input
+                    type="email"
+                    placeholder="Email Address"
+                    name="email"
+                    required
+                />
+                </div>
+                <div class="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                />
+                </div>
+                <input type="submit" class="btn btn-primary" value="Login" />
+            </form>
+            <p class="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
     )
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <Link to="index.html"><i className="fas fa-code"></i> BriefHub</Link>
+                <Link to="/"><i className="fas fa-code"></i> BriefHub</Link>
             </h1>
             <ul>
                 <li><Link to="/profiles">Profiles</Link></li>
