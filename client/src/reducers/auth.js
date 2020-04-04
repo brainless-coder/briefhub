7e74a413aee1687adfd0b19bef7acca1a4d9cd2b
Aditya Tomar <aditya001tomar@gmail.com>
2020-04-05 02:06:23 +0530

    set auth token in react

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -13,6 +15,13 @@ const initialState = {
 export default function(state = initialState, action) {
     const {type, payload } = action;
     switch(type){
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
             localStorage.setItem('item', payload.token);
             return{
@@ -23,6 +32,7 @@ export default function(state = initialState, action) {
             }
         
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token');
             return{
                 ...state,
